82a5ee31bcf26dbdc62fc5da5d50b8be1a35480e
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-10-12 21:28:51 +0900

    S3にtfstateを配置

--- backend.tf
+++ backend.tf
@@ -1,19 +1,19 @@
 # tfstate をローカル管理する場合
-terraform {
-  backend "local" {
-    path = "./terraform.tfstate"
-  }
-}
-
-# tfstate をS3管理する場合
 # terraform {
-#   backend "s3" {
-#     bucket  = "bucket-name"
-#     region  = "us-east-1"
-#     key     = "dir/terraform.tfstate"
-#     encrypt = false
-# S3のAWSアカウントがデプロイ先と異なる場合に指定
-#    access_key = ""
-#    secret_key = ""
+#   backend "local" {
+#     path = "./terraform.tfstate"
 #   }
 # }
+
+# tfstate をS3管理する場合
+terraform {
+  backend "s3" {
+    bucket  = "backend-bucket" # 格納先のバケット名に変更
+    region  = "ap-northeast-1"
+    key     = "hands_on1/terraform.tfstate"
+    encrypt = false
+# S3のAWSアカウントがデプロイ先と異なる場合に指定
+#   access_key = """
+#   secret_key = ""
+  }
+}
